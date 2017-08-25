2c8aadad72c0197e31ea7ca2c391aefcd7a9c5f8
Andrew <awilom6175@greenvilleschools.us>
2017-08-25 09:48:02 -0400

    Initial sharing of project

---
+++ Introduction.java
@@ -0,0 +1,29 @@
+/**
+ * This prgram tells a few things about meeeee.
+ * 
+ * @Andrew Wilomovkys 
+ * @version (tbh more of a date but what ever b) August 23, 2017
+ *****************************************************************/
+ public class Introduction
+ {
+     public static void main(String[] args)
+     {
+     //Some facts about me
+     System.out.println("My name is Andrew J. Wilomovsky");
+     System.out.println("I was born in NYC Long Island");
+     System.out.println("I am going to JL mann high school");
+     System.out.println("My mother is, Hope and she is 49");
+     System.out.println("I like to camp, hike, and game");
+     
+     // Now store some numbers
+     
+     int year = 2017;
+     int age = 16;
+     
+     //What happens here?
+     System.out.println("Year - age");
+     System.out.println(year - age);
+     System.out.println("I am " + age + " years old. ");
+    }
+}
+     
